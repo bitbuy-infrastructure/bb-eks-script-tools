6c457388c6ba31b0db2a8133f74891eb5dcd9547
henrique-zis <henrique.zis@salesforce.com>
2023-02-23 11:37:13 -0300

    fixing dms replication task completed between full-load and full-load-and-cdc

--- dms/dms.sh
+++ dms/dms.sh
@@ -30,14 +30,13 @@ fi
 if [ -z $new_user_pwd ]; then
 	echo "Please setup the new_user_pwd of user "$new_username" from db_host: "$new_db_host; exit;
 fi
-
 log echo "### Migrate env "$1" db "$2" ###"
 log echo "Creating source endpoint... "
-source_endpoint_arn=$(aws dms create-endpoint --endpoint-identifier from-postgres-$1-db-$2 --endpoint-type source --engine-name postgres \
+source_endpoint_arn=$(aws dms create-endpoint --endpoint-identifier from-postgres-$1-db-`echo $2 | tr '_' '-'` --endpoint-type source --engine-name postgres \
 --username $old_master_user --password $old_master_pwd --server-name $old_db_hostname --port 5432 --database-name $2 | jq -r ."Endpoint"."EndpointArn")
 log echo $source_endpoint_arn
 log echo "Creating target endpoint... "
-target_endpoint_arn=$(aws dms create-endpoint --endpoint-identifier to-aurora-$1-db-$new_db_name --endpoint-type target --engine-name aurora-postgresql \
+target_endpoint_arn=$(aws dms create-endpoint --endpoint-identifier to-aurora-$1-db-`echo $new_db_name | tr '_' '-'` --endpoint-type target --engine-name aurora-postgresql \
 --username $new_username --password $new_user_pwd --server-name $new_db_host --port 5432 --database-name $new_db_name | jq -r ."Endpoint"."EndpointArn")
 log echo $target_endpoint_arn
 
@@ -52,9 +51,9 @@ while [[ $source_endpoint_status != \"successful\" ]] && [[ $target_endpoint_sta
 done
 aws dms describe-connections --filter "Name=endpoint-arn,Values=$source_endpoint_arn,$target_endpoint_arn"
 log echo "Creating replication task..."
-aws dms create-replication-task --replication-task-identifier migrate-$1-postgres-db-to-aurora-$2-db --source-endpoint-arn $source_endpoint_arn --target-endpoint-arn $target_endpoint_arn \
+aws dms create-replication-task --replication-task-identifier migrate-$1-postgres-db-to-aurora-`echo $2 | tr '_' '-'`-db --source-endpoint-arn $source_endpoint_arn --target-endpoint-arn $target_endpoint_arn \
 --replication-instance-arn $rep_instance_arn --migration-type $migration_type --table-mappings file://table-mappings --replication-task-settings file://task-settings
-replication_task_arn=$(aws dms describe-replication-tasks --filters "Name= replication-task-id,Values=migrate-$1-postgres-db-to-aurora-$2-db" --query "ReplicationTasks[0].ReplicationTaskArn" --output text)
+replication_task_arn=$(aws dms describe-replication-tasks --filters "Name= replication-task-id,Values=migrate-$1-postgres-db-to-aurora-`echo $2 | tr '_' '-'`-db" --query "ReplicationTasks[0].ReplicationTaskArn" --output text)
 log echo $replication_task_arn
 
 while [[ $status != \"ready\" ]]; do
@@ -80,6 +79,11 @@ while [[ $status != \"stopped\" ]]; do
 			log echo "### Replication failed! Reason: "$finish" ###"
 		fi
 	else
+		full_load=$(aws dms describe-replication-tasks --filters "Name=replication-task-arn,Values=$replication_task_arn" --query "ReplicationTasks[0].ReplicationTaskStats.FullLoadFinishDate")
+		if [ $status == \"running\" ] && [ $migration_type == 'full-load-and-cdc' ] && [ ! -z $full_load ]; then
+			log echo "### Load completed! Replication ongoing! Migration process completed. Check the tables. Ex: select count(*) from <table_name>; ###"
+			exit;
+		fi
 		sleep 3
 		echo "Current task status: "$status " Waiting for task get completed"
 	fi
